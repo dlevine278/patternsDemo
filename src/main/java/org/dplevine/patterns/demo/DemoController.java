5070c83a7321fa089537d676e2ee0acf046c384a
dplevine <dplevine@gmail.com>
2023-10-24 16:27:34 -0400

    got a basic demo going

--- src/main/java/org/dplevine/patterns/demo/DemoController.java
+++ src/main/java/org/dplevine/patterns/demo/DemoController.java
@@ -1,11 +1,16 @@
 package org.dplevine.patterns.demo;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.dplevine.patterns.demo.pipelineDemo.PipelineDemo;
+import org.dplevine.patterns.pipeline.Pipeline;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -18,10 +23,11 @@ import java.util.concurrent.ConcurrentMap;
 @RestController
 public class DemoController {
     private static int MAX_DEMOS = 1000;
-    private static long MAX_TIME_SECONDS = 60 * 1;  // demos can stick around for 15 min before they get purged
+    private static long MAX_TIME_SECONDS = 60 * 15;  // demos can stick around for 15 min before they get purged
 
 
-    DemoController() {}
+    DemoController() {
+    }
 
     private static class ActiveDemos {
         private Map<String, Object> demoInstances = new ConcurrentHashMap<>();
@@ -91,16 +97,6 @@ public class DemoController {
         return "list";
     }
 
-    @GetMapping(value = "/patterns/demos/pipeline")
-    public String startNewPipelineDemo() {
-        String uuid;
-        try {
-            return ActiveDemos.getDemos().addDemo("demo");
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-    }
-
 
     @RequestMapping(value = "/patterns/index", method = RequestMethod.GET)
     public void getDemolist(HttpServletResponse response) throws IOException {
@@ -113,6 +109,63 @@ public class DemoController {
             response.setContentType(MediaType.TEXT_PLAIN_VALUE);
             final PrintWriter pw = response.getWriter();
             pw.println("patterns");
+            pw.close();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    @RequestMapping(value = "/patterns/demos/pipeline")
+    public void startNewPipelineDemo(HttpServletRequest request, HttpServletResponse response) {
+        String uuid;
+        try {
+            PipelineDemo demo = new PipelineDemo();
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,X-PINGOTHER,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+            response.setHeader("Access-Control-Expose-Headers", "xsrf-token");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            final PrintWriter pw = response.getWriter();
+            pw.print( "{ \"id\" : \"" + ActiveDemos.getDemos().addDemo(demo) + "\" }");
+            demo.runDemo();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    @RequestMapping(value = "/patterns/demos/pipeline/graph/{id}", method = RequestMethod.GET)
+    public void getImageAsByteArray(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            PipelineDemo pipelineDemo = (PipelineDemo) ActiveDemos.getDemos().getActiveDemo(id);
+            BufferedImage image = pipelineDemo.getImage();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(image, "gif", os);
+            InputStream is = new ByteArrayInputStream(os.toByteArray());
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,X-PINGOTHER,Content-Type: image/gif,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+            response.setHeader("Access-Control-Expose-Headers", "xsrf-token");
+            response.setContentType(MediaType.IMAGE_GIF_VALUE);
+            IOUtils.copy(is, response.getOutputStream());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    @RequestMapping(value = "/patterns/demos/pipeline/log/{id}", method = RequestMethod.GET)
+    public void getEventLog(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            PipelineDemo pipelineDemo = (PipelineDemo) ActiveDemos.getDemos().getActiveDemo(id);
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,X-PINGOTHER,Content-Type: application/json;charset=UTF-8,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+            response.setHeader("Access-Control-Expose-Headers", "xsrf-token");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            final PrintWriter pw = response.getWriter();
+            pw.print(pipelineDemo.getLogs());
             pw.close();
         } catch (Exception e) {
             e.getMessage();
---
+++ src/main/java/org/dplevine/patterns/demo/pipelineDemo/PipelineDemo.java
@@ -0,0 +1,74 @@
+package org.dplevine.patterns.demo.pipelineDemo;
+
+
+import org.dplevine.patterns.pipeline.*;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.concurrent.Future;
+
+public class PipelineDemo implements Stage, StageBuilder {
+
+    static final String PIPLINE_GRAPH = "/pipelineDemo.json";
+    static final Long MAX_DELAY = 10L;
+    private Pipeline demoPipeline;
+
+    public static class TimerContext extends ExecutionContext {
+        public static final String MAX_DELAY_SECONDS = "MaxDelay";
+
+        public TimerContext() {
+            super();
+        }
+
+        public Long getMaxDelay() {
+            return (Long) getObject(MAX_DELAY_SECONDS);
+        }
+
+        public void setMaxDelay(Long maxDelay) {
+            addObject(MAX_DELAY_SECONDS, maxDelay);
+        }
+    }
+
+    public ExecutionContext doWork(ExecutionContext context) throws Exception {
+        PipelineDemo.TimerContext timerContext = (PipelineDemo.TimerContext) context;
+        long delay = Math.round(timerContext.getMaxDelay() * 1000 * Math.random());
+        Thread.sleep(Math.round(delay));
+
+        if (Math.round(Math.random()) == 1) {
+            throw new Exception("simulated exception");
+        }
+        return context;
+    }
+
+    @Override public Stage buildStage() {
+        return new PipelineDemo();
+    }
+
+    public void runDemo() throws Exception {
+        try {
+            PipelineBuilder builder = PipelineBuilder.createBuilder();
+
+            URL url = PipelineDemo.class.getResource(PIPLINE_GRAPH);
+            demoPipeline = builder.buildFromPathName(url.getPath());
+
+            TimerContext context = new TimerContext();
+            context.setMaxDelay(MAX_DELAY);
+            Future<ExecutionContext> future = demoPipeline.runDetached(context);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    public String getLogs() {
+        if (demoPipeline != null) {
+            return demoPipeline.getContext().getEventLog().toString();
+        }
+        return "";
+    }
+
+    public BufferedImage getImage() throws Exception {
+            return demoPipeline.render();
+    }
+
+
+}
